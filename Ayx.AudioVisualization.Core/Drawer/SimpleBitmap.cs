38e4e33b1854584350b3010426d9402f1f50a7e9
durow <durow@163.com>
2020-04-04 15:02:17 +0800

    添加项目文件。

---
+++ Ayx.AudioVisualization.Core/Drawer/SimpleBitmap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ayx.AudioVisualization.Core.Drawer
+{
+    public class SimpleBitmap
+    {
+        public byte[] Bitmap { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int GetIndex(int x, int y)
+        {
+            return x + y * Width;
+        }
+
+        public void DrawPixel(int x, int y, byte colorIndex)
+        {
+
+        }
+    }
+}
